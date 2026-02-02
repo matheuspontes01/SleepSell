2bb1a1d0d25a0a519bf375ee43f0a06ff32b8dd5
matheuspontes01 <matheus.pontes@mail.uft.edu.br>
2026-02-02 17:25:36 -0300

    Projeto spring criado

---
+++ src/main/java/com/vendas/SleepSell/SleepSellApplication.java
@@ -0,0 +1,13 @@
+package com.vendas.SleepSell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepSellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepSellApplication.class, args);
+	}
+
+}
